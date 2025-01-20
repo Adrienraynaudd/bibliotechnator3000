874c564717b14b5700b864e02b2319452ac22a65
PierreLepesant <pierre.lepesant@icilundi.fr>
2025-01-20 11:06:00 +0100

    updated imports

--- Api/src/routes/example.js
+++ Api/src/routes/example.js
@@ -1,17 +1,22 @@
 // routes/example.js
 
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-import { createExample, getExample, updateExample, deleteExample } from '../controller/example.js';
+import {
+  createExample,
+  getExample,
+  updateExample,
+  deleteExample,
+} from "../controllers/example.js";
 
 // this route create a new note
-router.post('/', createExample);
+router.post("/", createExample);
 // this route get all the notes by the user id
-router.get('/user/:userId', getExample);
+router.get("/user/:userId", getExample);
 // this route update one note by the note id
-router.put('/:id', updateExample);
+router.put("/:id", updateExample);
 // this route remote one note by the note id
-router.delete('/:id', deleteExample);
+router.delete("/:id", deleteExample);
 
 export default router;
