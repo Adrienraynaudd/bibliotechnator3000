905cd33a2b9ba31cf2fce4dd09a13c002ef085a4
PierreLepesant <pierre.lepesant@icilundi.fr>
2025-01-20 10:59:17 +0100

    added examples

---
+++ Api/src/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import example_route from "./routes/example.js";
+
+dotenv.config();
+const app = express();
+
+console.clear();
+
+// Middleware pour le corps des requetes JSON
+app.use(express.json());
+
+// Utilisation des routes
+app.use("/example", example_route);
+
+app.listen(process.env.PORT, () => {
+//   logger.info(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
---
+++ Api/src/routes/example.js
@@ -0,0 +1,17 @@
+// routes/example.js
+
+import express from 'express';
+const router = express.Router();
+
+import { createExample, getExample, updateExample, deleteExample } from '../controller/example.js';
+
+// this route create a new note
+router.post('/', createExample);
+// this route get all the notes by the user id
+router.get('/user/:userId', getExample);
+// this route update one note by the note id
+router.put('/:id', updateExample);
+// this route remote one note by the note id
+router.delete('/:id', deleteExample);
+
+export default router;
