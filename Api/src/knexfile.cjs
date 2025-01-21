708c940e6d2692b18d12452499730177c9644a82
Lepetoc <lepetoctgp@outlook.fr>
2025-01-21 11:35:27 +0100

    knex configuration and github action

---
+++ Api/src/knexfile.cjs
@@ -0,0 +1,15 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: '../migrations',
+    },
+  },
+};
