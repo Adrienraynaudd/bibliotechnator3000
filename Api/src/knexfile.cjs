7a79cbc33fdb9a32309f95deb66b5444dcb2a04b
Lepetoc <lepetoctgp@outlook.fr>
2025-01-21 14:06:36 +0100

    database table migrations

--- Api/src/knexfile.cjs
+++ Api/src/knexfile.cjs
@@ -7,11 +7,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-<<<<<<< HEAD
     connection: 'postgres://admin:admin@localhost:5432/ultimatelibrary',
-=======
-    connection: process.env.DATABASE_URL,
->>>>>>> 2c16652 (knex configuration and github action)
     migrations: {
       directory: '../migrations',
     },
