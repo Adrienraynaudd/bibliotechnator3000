d796148c03d879dcd4935f0cf3498ab0f74f4a74
Paul <paul.piauger2@gmail.com>
2025-01-21 13:37:52 +0100

    detail doc debut

---
+++ App/src/app/documents/pages/detail/detail.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {DetailDocComponent} from '../../compnents/detail-doc/detail-doc.component';
+
+@Component({
+  selector: 'app-detail',
+  imports: [
+    DetailDocComponent
+  ],
+  templateUrl: './detail.component.html',
+  styleUrl: './detail.component.css'
+})
+export class DetailComponent {
+
+}
