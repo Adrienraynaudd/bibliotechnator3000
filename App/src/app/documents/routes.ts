9e729ea07240d826e607ab40b9d522e3ab891020
Paul <paul.piauger2@gmail.com>
2025-01-21 14:39:16 +0100

    lien vers detail

--- App/src/app/documents/routes.ts
+++ App/src/app/documents/routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { ListDocumentComponent } from './pages/list-document/list-document.component';
+import {DetailComponent } from './pages/detail/detail.component';
 
 export const routes: Routes = [
-    { path: ':id', component: ListDocumentComponent },
+    { path: '', component: ListDocumentComponent },
+    {path: ':id', component: DetailComponent},
 ];
