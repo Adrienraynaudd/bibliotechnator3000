3b36ef07c7fd7ff98d92c6d0ae3abc78b61b6730
adrienraynaudd <adrien48632@gmail.com>
2025-01-21 15:20:23 +0100

    uplaod delete marche

--- Api/src/index.js
+++ Api/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import example_route from "./routes/example.js";
+import document_route from "./routes/document.js";
 
 dotenv.config();
 const app = express();
@@ -14,7 +15,7 @@ app.use(express.json());
 app.use("/example", example_route);
 
 // Utilisation des routes
-app.use("/documents", example_route);
+app.use("/documents", document_route);
 
 app.listen(process.env.PORT, () => {
 //   logger.info(`Server is running on port ${process.env.PORT}`);
