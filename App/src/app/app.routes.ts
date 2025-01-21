206c4cdc5348bb202ceaee3743b82fdc7156bf0b
Luca Chaboissier <lucachaboissier@outlook.com>
2025-01-21 10:45:43 +0100

    fix: restructure pages

--- App/src/app/app.routes.ts
+++ App/src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { LibraryComponent } from './pages/library/library.component';
-import { QuizComponent } from './pages/quiz/quiz.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LibraryComponent } from './library/pages/library.component';
+import { QuizzComponent } from './quizz/pages/quizz.component';
+import { LoginComponent } from './auth/pages/login.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'library', component: LibraryComponent },
-  { path: 'quiz', component: QuizComponent },
+  { path: 'quizz', component: QuizzComponent },
   { path: 'login', component: LoginComponent },
 ];
--- App/src/app/quizz/pages/quizz.component.spec.ts
+++ App/src/app/quizz/pages/quizz.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { QuizComponent } from './quiz.component';
+import { QuizzComponent } from './quizz.component';
 
-describe('QuizComponent', () => {
-  let component: QuizComponent;
-  let fixture: ComponentFixture<QuizComponent>;
+describe('QuizzComponent', () => {
+  let component: QuizzComponent;
+  let fixture: ComponentFixture<QuizzComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [QuizComponent]
+      imports: [QuizzComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(QuizComponent);
+    fixture = TestBed.createComponent(QuizzComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
