a9abdbbe9ec280cfb5c6c1584d151af2491dac08
MaxChevalier <maxime.chevalier@ynov.com>
2025-01-23 21:36:07 +0100

    ✨ add quizz and create quizz

--- App/src/app/app.routes.ts
+++ App/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { routes as documentsRoutes } from './documents/routes';
+import { routes as quizRoutes } from './quizz/routes';
 
 export const routes: Routes = [
     {path: 'documents', children: documentsRoutes},
+    {path: 'quizz', children: quizRoutes},
 ];
