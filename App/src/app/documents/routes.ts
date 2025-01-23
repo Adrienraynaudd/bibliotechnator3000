42139d3c3fb61d8ea03cc506fa75270d533296d4
Paul <paul.piauger2@gmail.com>
2025-01-23 18:43:40 +0100

    debut form ajout doc

--- App/src/app/documents/routes.ts
+++ App/src/app/documents/routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { ListDocumentComponent } from './pages/list-document/list-document.component';
 import {DetailComponent } from './pages/detail/detail.component';
+import {FormNewDocComponent } from './pages/form-new-doc/form-new-doc.component';
 
 export const routes: Routes = [
     { path: '', component: ListDocumentComponent },
     {path: ':id', component: DetailComponent},
+    {path: 'newdoc', component: FormNewDocComponent}
 ];
